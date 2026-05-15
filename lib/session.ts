854e71f75c31137e202c0a2e42c766a9d927fd26
mcs <learn>
2026-05-15 16:11:38 +0400

    start from the 0

--- lib/session.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from './auth';
-
-export async function getCurrentSession() {
-  return await getServerSession(authOptions);
-}
